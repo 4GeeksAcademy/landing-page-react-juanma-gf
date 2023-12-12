497059e433c5a922fe31d960e74df795ed18e882
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2023-12-12 13:42:47 +0000

    arreglar cards

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Card() {
     return (
-        <div className="card m-auto  mb-4"  >
+        <div className="card mb-4 "  >
             <img src="https://picsum.photos/id/34/500/325" className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title text-center">Card title</h5>
+                <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-            <div className="card-footer">
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+            <div className="card-footer text-center">
+                <a href="#" className="btn btn-primary  ">Go somewhere</a>
             </div>
         </div>
     );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 	return (
 		<div>
 		<Navbar/>
-		<div className="container-fluid mt-4 ">
+		<div className="container-fluid ">
 		<Jumbotron/>
 		<Card/>
 		<Card/>
